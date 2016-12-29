d33021c98610f5aaf7eeb42c8318e44702847f30
Simon Schwartz <simon.schwartz0@gmail.com>
2016-12-29 20:08:41 +1100

    add basic routing with vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
+
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+import AppHeader from './components/AppHeader.vue'
+import Home from './components/Home.vue'
+import Dashboard from './components/Dashboard.vue'
+
+var router = new VueRouter()
+
+var router = new VueRouter({
+  routes: [
+    { path: '/', component: Home },
+    { path: '/dashboard', auth: true, component: Dashboard },
+    { path: '*', redirect: '/home'}
+  ]
+})
+
 var vm = new Vue({
   el: '#app',
+  router: router,
   render: h => h(App),
 })
